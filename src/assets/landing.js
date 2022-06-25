29f1f39965ace06331426576b1ed20d71a9e5e0a
raydenleo <squallray@hotmail.com>
2022-06-26 01:11:56 +0800

    creating side menu

---
+++ src/assets/landing.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    //console.log("page is ready");
+});
+
+
+/*
+$(document).on("scroll", function() {
+    var pageTop = $(document).scrollTop();
+    var pageBottom = pageTop + $(window).height();
+    var tags = $(".homepage-sc .content-wrap");
+
+    for (var i = 0; i < tags.length; i++) {
+        var tag = tags[i];
+
+        if ($(tag).position().top < pageBottom) {
+        $(tag).addClass("visible");
+        } else {
+        $(tag).removeClass("visible");
+        }
+    }
+});*/
