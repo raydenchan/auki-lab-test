f9a5a3b8c760d95d8000f118c6ae1917cb34ed68
raydenleo <squallray@hotmail.com>
2022-06-27 01:41:17 +0800

    firebase setup

---
+++ src/firebase/config.js
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCp9MPeLcLwtQ1uYa1awe9rN14i8oS0jjI",
+  authDomain: "auki-lab-test.firebaseapp.com",
+  projectId: "auki-lab-test",
+  storageBucket: "auki-lab-test.appspot.com",
+  messagingSenderId: "323371593288",
+  appId: "1:323371593288:web:b9ac4fb5f4026c7813727d"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+export default 
+firebaseApp
+
+//firebase firestore:indexes
+//run this script to get settings for firestore.indexes.json
+
+
+
+
+
