391b43f0c00edb8e86f85da68031dff68a05fb10
raydenleo <squallray@hotmail.com>
2022-06-27 21:07:51 +0800

    final update

--- src/assets/landing.js
+++ src/assets/landing.js
@@ -1,21 +1,68 @@
+const Main = {
+
+    init: function(){
+        $("#secondaryMenuToggler").click(function(){
+            Main.openSideMenu();
+        });
+    
+        $("#secondaryMenuClose").click(function(){
+            Main.closeSideMenu();
+        });
+
+        $('aside#sideMenu a.nav-link').click(function(){
+            Main.closeSideMenu();
+        });
+
+        $('.sidemenu-overlay').click(function(){
+            Main.closeSideMenu();
+        });
+    },
+
+    openSideMenu: function (){
+        $('.sidemenu-overlay').show();
+        $('aside#sideMenu').show();
+        $('aside#sideMenu').removeClass('slide-out');
+        $('aside#sideMenu').addClass('slide-in');
+        $('body').addClass('noscroll'); 
+    },
+    closeSideMenu: function (){
+        $('.sidemenu-overlay').hide();
+        $('aside#sideMenu').removeClass('slide-in');
+        $('aside#sideMenu').addClass('slide-out');
+        $('body').removeClass('noscroll'); 
+    },
+
+    checkAnimation: function (){
+        $('section.homepage-sc .content-wrap').each(function(){
+            let e = $(this);
+            if(Main.elementOnScreen(e)){
+                e.addClass('fadeIn');
+            } else {
+                e.removeClass('fadeIn');
+            }
+                
+        });
+    },
+
+    elementOnScreen: function (e){
+
+        let eTop = e.offset().top;
+        let eBottom = eTop + e.outerHeight();
+        let vpTop = $(window).scrollTop();
+        let vpBottom = vpTop + $(window).height();
+
+        return eBottom > vpTop && eTop < vpBottom;
+    }
+
+}
+
+
 
 $(document).ready(function(){
-
-    $("#secondaryMenuToggler").click(function(){
-        $('.sidemenu-overlay').show();
-        $('aside.sidemenu-wrapper').show();
-        $('aside.sidemenu-wrapper').removeClass('slide-out');
-        $('aside.sidemenu-wrapper').addClass('slide-in');
-        $('html').addClass('noscroll');
-    });
-
-
-    $("#secondaryMenuClose").click(function(){
-        $('.sidemenu-overlay').hide();
-        $('aside.sidemenu-wrapper').removeClass('slide-in');
-        $('aside.sidemenu-wrapper').addClass('slide-out');
-        $('html').removeClass('noscroll');
-    });
-
-
+    Main.init();
 })
+
+
+$(window).on('scroll', function(e) {
+    Main.checkAnimation();
+});
